029f4ca766bbd702ffb1cb38bc8a4c9f663d9226
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-10-26 19:36:37 -0300

    Requirement two

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      as: 'blog_posts',
+      foreignKey: 'userId',
+    });
+  };
+
+  return User;
+};
